5faad34f97bac4c21bf0df7074db0adca548f214
Man Le <man.le@executionlab.asia>
2021-03-09 16:49:48 +0700

    fix scope

--- lib/google_sign_in_dartio.dart
+++ lib/google_sign_in_dartio.dart
@@ -118,7 +118,7 @@ class GoogleSignInDart extends platform.GoogleSignInPlatform {
     //     'for a list of valid OAuth 2.0 scopes.');
 
     if (scopes.isEmpty) {
-      _scopes = const <String>['openid', 'email', 'profile'];
+      _scopes = const <String>['email', 'profile'];
     } else {
       _scopes = scopes;
     }
